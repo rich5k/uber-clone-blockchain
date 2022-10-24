084d1507b8840bbab3ab1fa1fdd0bfb8be4f5645
rich5k <richardanatsui@gmail.com>
2022-10-24 02:45:50 +0100

    created navbar

---
+++ components/Navbar.js
@@ -0,0 +1,25 @@
+import Image from "next/image"
+import avatar from "../temp/avatar.png"
+const style={
+    wrapper: 'h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20'
+}
+const Navbar= ()=>{
+    return <div className={style.wrapper}>
+        <div className={style.leftMenu}>
+            <div className={style.logo}>Uber</div>
+            <div className={style.menuItem}>Ride</div>
+            <div className={style.menuItem}>Drive</div>
+            <div className={style.menuItem}>More</div>
+        </div>
+        <div className={style.rightMenu}>
+        <div className={style.menuItem}>Help</div>
+        <div className={style.menuItem}>Richard</div>
+        <div className={style.userImageContainer}>
+            <Image src={avatar} width={40} height={40}/>
+        </div>
+        </div>
+    </div>
+}
+
+export default Navbar
+ 
