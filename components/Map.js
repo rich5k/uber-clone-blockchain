c905b12fabbc856e208d6c21e8e39af3b97f4846
rich5k <richardanatsui@gmail.com>
2022-11-07 00:42:52 +0000

    added mapbox-gl and its styles link

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,17 @@
-import React from "react";
+import { useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+const style = {
+    wrappers: 'flex-1 h-full w-full'
+}
 
 const Map=()=>{
-    return <div>Map</div>
+    useEffect(()=>{
+        const map = new mapboxgl.Map({
+            style: 'mapbox://styles/rich5k/cla621lqi001i14nsh6simczp'
+        })
+    }, [])
+    return <div className={style.wrappers}>Map</div>
 }
 
 export default Map;
