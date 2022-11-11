d48132a653af95f5782a9159e4ee884351a6b3bc
rich5k <richardanatsui@gmail.com>
2022-11-11 06:59:53 +0000

    added mapbox access token and improved map styles

--- components/Map.js
+++ components/Map.js
@@ -2,16 +2,21 @@ import { useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 
 const style = {
-    wrappers: 'flex-1 h-full w-full'
+    wrapper: 'flex-1 h-full w-full'
 }
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
 const Map=()=>{
     useEffect(()=>{
         const map = new mapboxgl.Map({
-            style: 'mapbox://styles/rich5k/cla621lqi001i14nsh6simczp'
+            container: 'map',
+            style: 'mapbox://styles/rich5k/cla621lqi001i14nsh6simczp',
+            center: [-99.29011, 39.39172],
+            zoom: 3,
         })
     }, [])
-    return <div className={style.wrappers}>Map</div>
+    return <div className={style.wrapper} id='map' />
 }
 
 export default Map;
