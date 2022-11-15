8f98d7b01b72bc9db60759a0aad0d5d211def46f
rich5k <richardanatsui@gmail.com>
2022-11-15 02:33:08 +0000

    added location selector

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import LocationSelector from "../components/LocationSelector"
 import Map from "../components/Map"
 import Navbar from "../components/Navbar"
 
@@ -5,7 +6,7 @@ const style = {
   wrapper: 'h-screen w-screen flex flex-col',
   main: 'h-full w-screen flex-1 z-10',
   mapContainer: 'flex-1 w-full h-full',
-  rideRequestContainer: 'h-full w-[400px] m1-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end',
+  rideRequestContainer: 'h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20',
   rideRequest: 'h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll'
 }
 
@@ -14,12 +15,12 @@ export default function Home() {
     <div className={style.wrapper}>
       <Navbar />
       <div className={style.main}>
-        <Map />
+        <Map  />
       </div>
       <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>
           <div>
-            {/* location selector */}
+            <LocationSelector />
             {/* confirm ride */}
           </div>
         </div>
